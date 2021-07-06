06915c48b2e96920069b2294a24bf8ddb0f9ae8f
Nekesa <Nyongesacharity13@gmail.com>
2021-07-06 17:39:42 +0300

    Consuming API data in flask

--- app.py
+++ app.py
@@ -1,68 +1,44 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import requests
-import json
-from flask_mysqldb import MySQL
 
-app = Flask("__main__")
+app = Flask(__name__)
 
-app.config["MYSQL_HOST"] = "127.0.0.1"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "employee"
+db = SQLAlchemy(app)
 
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-@app.route("/", methods=['GET'])
-def homepage():
-    r = requests.get("https://richardobiye.com/py/api/employee-details.php")
-    myresponse = r.content
-    res_to_json = json.loads(myresponse)
-    cur = mysql.connection.cursor()
+class Employee(db.Model):
+     idNumbers = db.Column(db.Integer(), primary_key=True)
+     name = db.Column(db.String(50))
+     age = db.Column(db.Integer())
+     sex = db.Column(db.String(10))
+     maritalStatus = db.Column(db.String(50))
+     dateOfBirth = db.Column(db.Integer())
+     
 
-    
+app = Flask(__name__)
 
-    for employee in res_to_json:
-        idNumbers = employee['idNumbers']
-        name = employee['name']
-        age = employee['age']
-        sex = employee['sex']
-        maritalStatus = employee['maritalStatus']
-        dateOfBirth = employee['dateOfBirth']
-        laptop = employee['laptop']
-        ram = laptop['ram']
-        ssd = laptop['ssd']
-        storage = laptop['storage']
-        screenSize = laptop['screenSize']
-        speed = laptop['speed']
-        phones = employee['phones']
-        model = phones['model']
-        price = phones['price']
-        imei = phones['imei']
+@app.route('/', methods=['GET'])
 
-        spec = phones['spec']
-        ram = spec['ram']
-        camera = spec['camera']
+def get_users():
+
+     r = requests.get('http://richardobiye.com/py/api/employee.php')
+
+     data = r.json()
+     new_user = Employee(idNumbers = data['idNumbers'], name=data['name'], age=data['age'], sex=data['sex'], maritalStatus=data['maritalStatus'])
+
+     db.session.add(new_user)
+     
+     db.session.commit()
+
+     return jsonify({'message': 'User created'})
+
+   
+
+     
 
 
 
-        cur.execute("INSERT into emp(idNumbers,name,age,sex,maritalStatus,dateOfBirth) VALUES(%s,%s,%s,%s,%s,%s)",
-         (idNumbers, name, age, sex, maritalStatus, dateOfBirth))
-
-        cur.execute("INSERT into laptop(idNumbers,ram,ssd,storage,screenSize,speed) VALUES(%s,%s,%s,%s,%s,%s) ",
-                    (idNumbers, ram, ssd, storage, screenSize, speed))
-
-        cur.execute("INSERT into phones(idNumbers, model, price, imei) VALUES(%s, %s, %s, %s)",
-                    (idNumbers, model, price, imei))
-        cur.execute("INSERT into spec(idNumbers,ram, camera) VALUES(%s, %s, %s)",
-                    (idNumbers, ram, camera))
-
-        mysql.connection.commit()
-
-    cur.close()
-
-
-       
-
-
-
-
+if __name__ =='__main__':
+    app.run(debug=True)
